284e39f0bc118b4631c7e799de18f1e09562907a
Sait Can Tunç <tuncsaitcan@gmail.com>
2021-02-04 14:37:05 +0300

    4.02.2021 Ders Sonu!

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    //Generic constraint
+    //class : referans tip olabilir
+    //IEntity: IEntity ya da IEntity implemente olan bir nesne olabilir.
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); //Get'i kategorize etmek için Expression kullandık.
+        T Get(Expression<Func<T, bool>> filter); //Burada filtre vermek zorunda olduğumuz için filter kısmını null yapmadık
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
