e861ca1231d7d7e924dc58f930092632d1695311
Sait Can Tunç <tuncsaitcan@gmail.com>
2021-02-25 14:28:24 +0300

    25 Şubat Ders Sonu!

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics) // Params >> İstediğimiz kadar IResult atamamızı ve kullanmamızı sağlar.
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
+    }
+}
